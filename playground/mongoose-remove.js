c9d43e925404d3f31416c167a6e1ebcf783edfe4
Days0fThnder <j_rusangiza@yahoo.com>
2018-07-06 22:13:25 -0400

    Test DELETE /todos/:id route

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,12 @@
+const {ObjectID} = require('mongodb');
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+// Todo.remove({}).then((result) => {
+//     console.log(result);
+// });
+
+Todo.findByIdAndRemove('5b400c8e67b38b99a7362cb4').then((todo) => {
+    console.log(todo);
+});
